1256966eee326ad1a33054e42c86b5acaf60e389
BlasterCel <estudospessoaisdaceline@gmail.com>
2026-02-14 18:35:00 -0300

    ex6

---
+++ ex6/ex6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string resp = "N";
+
+        while (resp != "S")
+        {
+            Console.Write("Horas trabalhadas: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int e = 0;
+            int sal = n * 10;
+
+            if (n > 50)
+            {
+                e = (n - 50) * 10;
+                sal = sal + e;
+            }
+
+            Console.WriteLine($"Total: R${sal}");
+            Console.WriteLine($"Excedente: R${e}");
+
+            Console.Write("Encerrar? S/N: ");
+            resp = Console.ReadLine().ToUpper();
+        }
+    }
+}
