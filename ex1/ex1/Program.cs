44439c292d1e468b95860e90e319188978d3adce
BlasterCel <estudospessoaisdaceline@gmail.com>
2026-02-14 18:32:03 -0300

    ex1

---
+++ ex1/ex1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Números pares de 211 a 250:");
+
+        for (int a1 = 212; a1 <= 250; a1 += 2)
+        {
+            Console.WriteLine(a1);
+        }
+    }
+}N
