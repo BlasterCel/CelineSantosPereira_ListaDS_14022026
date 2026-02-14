6f121d58695187af7221e48e55048ba08db25e48
BlasterCel <estudospessoaisdaceline@gmail.com>
2026-02-14 18:33:51 -0300

    ex4

---
+++ ex4/ex4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double soma = 0;
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.Write($"Nota {i}: ");
+            double not = double.Parse(Console.ReadLine());
+
+            if (not < 0 || not > 10)
+            {
+                Console.WriteLine("Nota errada! Digite 0 a 10.");
+                i--;
+            }
+            else
+            {
+                soma += not;
+            }
+        }
+
+        Console.WriteLine($"Média: {soma / 10}");
+        Console.ReadKey();
+    }
+}
