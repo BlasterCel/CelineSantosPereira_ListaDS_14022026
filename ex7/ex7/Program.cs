53a5db2b03f13a3ff1044943d057841d689483b0
BlasterCel <estudospessoaisdaceline@gmail.com>
2026-02-14 20:27:10 -0300

    ex7

---
+++ ex7/ex7/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string resp = "N";
+
+        while (resp != "S")
+        {
+            Console.Write("Usuário 1 - Dia: ");
+            int d1 = int.Parse(Console.ReadLine());
+            Console.Write("Usuário 1 - Mês: ");
+            int m1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Usuário 2 - Dia: ");
+            int d2 = int.Parse(Console.ReadLine());
+            Console.Write("Usuário 2 - Mês: ");
+            int m2 = int.Parse(Console.ReadLine());
+
+            if (m1 < m2)
+                Console.WriteLine("Usuário 1 primeiro");
+            else if (m2 < m1)
+                Console.WriteLine("Usuário 2 primeiro");
+            else if (d1 < d2)
+                Console.WriteLine("Usuário 1 primeiro");
+            else if (d2 < d1)
+                Console.WriteLine("Usuário 2 primeiro");
+            else
+                Console.WriteLine("Mesmo dia");
+
+            Console.Write("\nEncerrar? S/N: ");
+            resp = Console.ReadLine().ToUpper();
+        }
+    }
+}
