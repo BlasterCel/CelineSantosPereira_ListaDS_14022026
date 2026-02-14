1f01b134346393b08f291d0e10652ab2806b6ad3
BlasterCel <estudospessoaisdaceline@gmail.com>
2026-02-14 18:33:14 -0300

    ex3

---
+++ ex3/ex3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int ma = 0;
+
+        for (int i = 1; i <= 15; i++)
+        {
+            Console.Write($"Número {i}: ");
+            int n = int.Parse(Console.ReadLine());
+
+            if (i == 1 || n > ma)
+            {
+                ma = n;
+            }
+        }
+
+        Console.WriteLine($"Maior número: {ma}");
+        Console.ReadKey();
+    }
+}
