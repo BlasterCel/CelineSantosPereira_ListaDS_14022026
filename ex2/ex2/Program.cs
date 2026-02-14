cdbc5cff546c6b2758a3a4a6d2cdc3f416449e8f
BlasterCel <estudospessoaisdaceline@gmail.com>
2026-02-14 18:32:45 -0300

    ex2

---
+++ ex2/ex2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o primeiro número: ");
+        int n1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Digite o segundo número: ");
+        int n2 = int.Parse(Console.ReadLine());
+
+        int me = n1;
+        int ma = n2;
+
+        if (n1 > n2)
+        {
+            me = n2;
+            ma = n1;
+        }
+
+        int contador = 0;
+
+        for (int i = me; i <= ma; i++)
+        {
+            if (i % 2 != 0)
+            {
+                contador++;
+            }
+        }
+
+        Console.WriteLine("Quantidade de ímpares: " + contador);
+        Console.ReadKey();
+    }
+}
